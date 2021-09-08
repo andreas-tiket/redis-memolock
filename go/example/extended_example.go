542b859da7d713720e3cb067242786fc72f3ef96
andreas-tiket <andreas.cendranata@tiket.com>
2021-09-08 16:17:01 +0700

    add singleflight and ristretto

--- go/example/extended_example.go
+++ go/example/extended_example.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
-	"github.com/gorilla/mux"
-	"github.com/kristoff-it/redis-memolock/go/memolock"
 	"net/http"
 	"os/exec"
 	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/mux"
+	"github.com/kristoff-it/redis-memolock/go/memolock"
 )
 
 // BindAddr contains the address for the HTTP Server to bind to.
 const BindAddr = "127.0.0.1:8080"
 
 func main() {
+	memolock.InitLocalCache(nil)
 	r := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379", // use default Addr
 		Password: "",               // no password set
@@ -28,14 +30,14 @@ func main() {
 	//  query:foo
 	//  query/lock:foo
 	//  query/notif:foo
-	queryMemolock, _ := memolock.NewRedisMemoLock(r, "query", 5*time.Second)
+	queryMemolock, _ := memolock.NewRedisMemoLock(r.Context(), r, "query", 5*time.Second)
 
 	// GET query/simple
 	router.HandleFunc("/query/simple/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := mux.Vars(r)["id"] // extract {id} from the url path
 
 		requestTimeout := 10 * time.Second
-		cachedQueryset, _ := queryMemolock.GetResource(id, requestTimeout, func() (string, time.Duration, error) {
+		cachedQueryset, _ := queryMemolock.GetResource(r.Context(), id, requestTimeout, func() (string, time.Duration, error) {
 			fmt.Printf("(query/queryset/%s) Working hard!\n", id)
 
 			// Simulate some hard work like fecthing data from a DBMS
@@ -51,14 +53,14 @@ func main() {
 	//  report:foo
 	//  report/lock:foo
 	//  report/notif:foo
-	reportMemolock, _ := memolock.NewRedisMemoLock(r, "report", 5*time.Second)
+	reportMemolock, _ := memolock.NewRedisMemoLock(r.Context(), r, "report", 5*time.Second)
 
 	// GET report/renewable
 	router.HandleFunc("/report/renewable/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := mux.Vars(r)["id"] // extract {id} from the url path
 
 		requestTimeout := 10 * time.Second
-		reportPdfLocation, _ := reportMemolock.GetResourceRenewable(id, requestTimeout, func(renew memolock.LockRenewFunc) (string, time.Duration, error) {
+		reportPdfLocation, _ := reportMemolock.GetResourceRenewable(r.Context(), id, requestTimeout, func(renew memolock.LockRenewFunc) (string, time.Duration, error) {
 			fmt.Printf("(report/renewable/%s) Working super-hard! (1)\n", id)
 			<-time.After(2 * time.Second)
 
@@ -81,7 +83,7 @@ func main() {
 		id := mux.Vars(r)["id"] // extract {id} from the url path
 
 		requestTimeout := 10 * time.Second
-		reportPdfLocation, err := reportMemolock.GetResourceRenewable(id, requestTimeout, func(renew memolock.LockRenewFunc) (string, time.Duration, error) {
+		reportPdfLocation, err := reportMemolock.GetResourceRenewable(r.Context(), id, requestTimeout, func(renew memolock.LockRenewFunc) (string, time.Duration, error) {
 			fmt.Printf("(report/oh-no/%s) Working super-hard! (1)\n", id)
 			<-time.After(6 * time.Second)
 
@@ -111,7 +113,7 @@ func main() {
 	//  ext:foo
 	//  ext/lock:foo
 	//  ext/notif:foo
-	extMemolock, err := memolock.NewRedisMemoLock(r, "ext", 15*time.Second)
+	extMemolock, err := memolock.NewRedisMemoLock(r.Context(), r, "ext", 15*time.Second)
 	if err != nil {
 		panic(err)
 	}
@@ -120,7 +122,7 @@ func main() {
 		word := mux.Vars(r)["word"] // extract {word} from the url path
 
 		requestTimeout := 10 * time.Second
-		stemming, _ := extMemolock.GetResourceExternal(word, requestTimeout, func() error {
+		stemming, _ := extMemolock.GetResourceExternal(r.Context(), word, requestTimeout, func() error {
 			fmt.Printf("(ext/stemmer/%s) Working hard!\n", word)
 
 			// We don't .Output() / try to read stdout, because we will be notified from Redis.
--- go/go.mod
+++ go/go.mod
@@ -3,7 +3,11 @@ module github.com/kristoff-it/redis-memolock/go
 go 1.12
 
 require (
+	github.com/dgraph-io/ristretto v0.1.0
 	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/go-redis/redis/v8 v8.11.3
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/gorilla/mux v1.7.1
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
